6a7c9b7c72e878a9cb32996a82fad738c3cb7fe3
Adam Fish Rybicki <adamrybicki@adams-macbook-pro.home>
2023-04-06 14:34:00 +0200

    derrived snakePickle to separete object

--- src/main/scala/sttp/openapi/models/requests/ModelsGetResponseData.scala
+++ src/main/scala/sttp/openapi/models/requests/ModelsGetResponseData.scala
@@ -37,30 +37,9 @@ object ModelsGetResponseData {
   }
 
   case class ModelsResponse(`object`: String, data: Seq[Data])
+
   object ModelsResponse {
     implicit def modelsResponseReadWriter: ReadWriter[ModelsResponse] =
       SnakePickle.macroRW[ModelsResponse].asInstanceOf[ReadWriter[ModelsResponse]]
-  }
-
-  object SnakePickle extends upickle.AttributeTagged {
-    private def camelToSnake(s: String): String =
-      s.replaceAll("([A-Z])", "#$1").split('#').map(_.toLowerCase).mkString("_")
-
-    private def snakeToCamel(s: String): String = {
-      val res = s.split("_", -1).map(x => s"${x(0).toUpper}${x.drop(1)}").mkString
-      s"${s(0).toLower}${res.drop(1)}"
-    }
-
-    override def objectAttributeKeyReadMap(s: CharSequence): String =
-      snakeToCamel(s.toString)
-
-    override def objectAttributeKeyWriteMap(s: CharSequence): String =
-      camelToSnake(s.toString)
-
-    override def objectTypeKeyReadMap(s: CharSequence): String =
-      snakeToCamel(s.toString)
-
-    override def objectTypeKeyWriteMap(s: CharSequence): String =
-      camelToSnake(s.toString)
   }
 }
