c7281148462410068af0f57515690bfc97a78141
Adam Fish Rybicki <adamrybicki@adams-macbook-pro.home>
2023-04-11 08:18:31 +0200

    removed my api key

--- src/main/scala/sttp/openai/Main.scala
+++ src/main/scala/sttp/openai/Main.scala
@@ -6,7 +6,7 @@ import sttp.openai.models.requests.ModelsGetResponseData.ModelsResponse
 object Main extends App {
   val backend: SyncBackend = DefaultSyncBackend()
 
-  val openApi: OpenAi = new OpenAi("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")
+  val openApi: OpenAi = new OpenAi("test")
   val response: Response[Either[ResponseException[String, Exception], ModelsResponse]] =
     openApi.getModels
       .send(backend)
