304f5cd6a8d416563d4787fff593355cdb868b2d
Adam Rybicki <37350527+flsh86@users.noreply.github.com>
2023-04-17 08:09:38 +0200

    Request retrieve model (#23)
    * Retrieve model endpoint implementation
    
    * removed commented import
    
    * uri from Upper to lower
    
    ---------
    
    Co-authored-by: Adam Fish Rybicki <adamrybicki@adams-macbook-pro.home>

--- core/src/main/scala/sttp/openai/OpenAi.scala
+++ core/src/main/scala/sttp/openai/OpenAi.scala
@@ -2,7 +2,7 @@ package sttp.openai
 
 import sttp.client4._
 import sttp.model.Uri
-import sttp.openai.requests.models.ModelsGetResponseData.ModelsResponse
+import sttp.openai.requests.models.ModelsResponseData.{ModelData, ModelsResponse}
 import sttp.openai.json.SttpUpickleApiExtension.asJsonSnake
 import sttp.openai.requests.files.FilesResponseData._
 
@@ -11,8 +11,18 @@ class OpenAi(authToken: String) {
   /** Fetches all available models from [[https://platform.openai.com/docs/api-reference/models]] */
   def getModels: Request[Either[ResponseException[String, Exception], ModelsResponse]] =
     openApiAuthRequest
-      .get(OpenAIEndpoints.ModelEndpoint)
+      .get(OpenAIEndpoints.modelEndpoint)
       .response(asJsonSnake[ModelsResponse])
+
+  /** @param modelId
+    *   a Model's Id as String
+    *
+    * Fetches an available model for given modelId from [[https://platform.openai.com/docs/api-reference/models/{modelId}]]
+    */
+  def retrieveModel(modelId: String): Request[Either[ResponseException[String, Exception], ModelData]] =
+    openApiAuthRequest
+      .get(OpenAIEndpoints.RetrieveModelEndpoint(modelId))
+      .response(asJsonSnake[ModelData])
 
   /** Fetches all files that belong to the user's organization from [[https://platform.openai.com/docs/api-reference/files]] */
   def getFiles: Request[Either[ResponseException[String, Exception], FilesResponse]] =
@@ -25,6 +35,7 @@ class OpenAi(authToken: String) {
 }
 
 private object OpenAIEndpoints {
-  val ModelEndpoint: Uri = uri"https://api.openai.com/v1/models"
   val FilesEndpoint: Uri = uri"https://api.openai.com/v1/files"
+  val modelEndpoint: Uri = uri"https://api.openai.com/v1/models"
+  def RetrieveModelEndpoint(modelId: String): Uri = modelEndpoint.addPath(modelId)
 }
