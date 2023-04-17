92096c2b9120cb80524b9ea23da3a52424c9b175
andrzej <bil.andrzej0@gmail.com>
2023-04-17 06:09:45 +0200

    Upload file endpoint implementation

--- core/src/main/scala/sttp/openai/OpenAi.scala
+++ core/src/main/scala/sttp/openai/OpenAi.scala
@@ -6,6 +6,8 @@ import sttp.openai.requests.models.ModelsGetResponseData.ModelsResponse
 import sttp.openai.json.SttpUpickleApiExtension.asJsonSnake
 import sttp.openai.requests.files.FilesResponseData._
 
+import java.io.File
+
 class OpenAi(authToken: String) {
 
   /** Fetches all available models from [[https://platform.openai.com/docs/api-reference/models]] */
@@ -19,6 +21,27 @@ class OpenAi(authToken: String) {
     openApiAuthRequest
       .get(OpenAIEndpoints.FilesEndpoint)
       .response(asJsonSnake[FilesResponse])
+
+  /** Upload a file that contains document(s) to be used across various endpoints/features. Currently, the size of all the files uploaded by
+    * one organization can be up to 1 GB. Please contact OpenAI if you need to increase the storage limit.
+    * @param file
+    *   [[java.io.File File]] of the JSON Lines file to be uploaded. <p> If the purpose is set to "fine-tune", each line is a JSON record
+    *   with "prompt" and "completion" fields representing your
+    *   [[https://platform.openai.com/docs/guides/fine-tuning/prepare-training-data training examples]].
+    * @param purpose
+    *   The intended purpose of the uploaded documents. <p> Use "fine-tune" for Fine-tuning. This allows OpenAI to validate the format of
+    *   the uploaded file.
+    * @return
+    *   Uploaded file's basic information.
+    */
+  def uploadFile(file: File, purpose: String = "fine-tune"): Request[Either[ResponseException[String, Exception], FileData]] =
+    openApiAuthRequest
+      .post(OpenAIEndpoints.FilesEndpoint)
+      .multipartBody(
+        multipart("purpose", purpose),
+        multipartFile("file", file)
+      )
+      .response(asJsonSnake[FileData])
 
   private val openApiAuthRequest: PartialRequest[Either[String, String]] = basicRequest.auth
     .bearer(authToken)
--- core/src/test/scala/sttp/openai/requests/files/FilesResponseDataSpec.scala
+++ core/src/test/scala/sttp/openai/requests/files/FilesResponseDataSpec.scala
@@ -9,7 +9,7 @@ import sttp.openai.requests.files.FilesResponseData.{FileData, FilesResponse}
 import sttp.openai.requests.files.FilesResponseData.FilesResponse._
 
 class FilesResponseDataSpec extends AnyFlatSpec with Matchers with EitherValues {
-  "Given files response as Json" should "be properly deserialized to case class" in {
+  "Given list files response as Json" should "be properly deserialized to case class" in {
     // given
     val listFilesResponse = fixtures.FilesResponse.listFilesJsonResponse
     val expectedResponse = FilesResponse(
@@ -30,6 +30,28 @@ class FilesResponseDataSpec extends AnyFlatSpec with Matchers with EitherValues 
 
     // when
     val givenResponse: Either[Exception, FilesResponse] = SttpUpickleApiExtension.deserializeJsonSnake.apply(listFilesResponse)
+
+    // then
+    givenResponse.value shouldBe expectedResponse
+  }
+
+  "Given upload file response as Json" should "be properly deserialized to case class" in {
+    // given
+    val singleFileJsonResponse = fixtures.FilesResponse.singleFileJsonResponse
+    val expectedResponse =
+      FileData(
+        `object` = "file",
+        id = "file-tralala",
+        purpose = "fine-tune",
+        filename = "example.jsonl",
+        bytes = 44,
+        createdAt = 1681375533,
+        status = "uploaded",
+        statusDetails = None
+      )
+
+    // when
+    val givenResponse: Either[Exception, FileData] = SttpUpickleApiExtension.deserializeJsonSnake[FileData].apply(singleFileJsonResponse)
 
     // then
     givenResponse.value shouldBe expectedResponse
