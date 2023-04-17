6bbeb4a829399604f96083ac1cb2f541eb8df618
andrzej <bil.andrzej0@gmail.com>
2023-04-17 14:41:44 +0200

    Change upload file method signature

--- core/src/main/scala/sttp/openai/OpenAi.scala
+++ core/src/main/scala/sttp/openai/OpenAi.scala
@@ -7,6 +7,7 @@ import sttp.openai.json.SttpUpickleApiExtension.asJsonSnake
 import sttp.openai.requests.files.FilesResponseData._
 
 import java.io.File
+import java.nio.file.Paths
 
 class OpenAi(authToken: String) {
 
@@ -34,12 +35,72 @@ class OpenAi(authToken: String) {
     * @return
     *   Uploaded file's basic information.
     */
-  def uploadFile(file: File, purpose: String = "fine-tune"): Request[Either[ResponseException[String, Exception], FileData]] =
+  def uploadFile(file: File, purpose: String): Request[Either[ResponseException[String, Exception], FileData]] =
     openApiAuthRequest
       .post(OpenAIEndpoints.FilesEndpoint)
       .multipartBody(
         multipart("purpose", purpose),
         multipartFile("file", file)
+      )
+      .response(asJsonSnake[FileData])
+
+  /** Upload a file that contains document(s) to be used across various endpoints/features. Currently, the size of all the files uploaded by
+    * one organization can be up to 1 GB. Please contact OpenAI if you need to increase the storage limit.
+    *
+    * @param file
+    *   [[java.io.File File]] of the JSON Lines file to be uploaded and the purpose is set to "fine-tune", each line is a JSON record with
+    *   "prompt" and "completion" fields representing your
+    *   [[https://platform.openai.com/docs/guides/fine-tuning/prepare-training-data training examples]].
+    * @return
+    *   Uploaded file's basic information.
+    */
+  def uploadFile(file: File): Request[Either[ResponseException[String, Exception], FileData]] =
+    openApiAuthRequest
+      .post(OpenAIEndpoints.FilesEndpoint)
+      .multipartBody(
+        multipart("purpose", "fine-tune"),
+        multipartFile("file", file)
+      )
+      .response(asJsonSnake[FileData])
+
+  /** Upload a file that contains document(s) to be used across various endpoints/features. Currently, the size of all the files uploaded by
+    * one organization can be up to 1 GB. Please contact OpenAI if you need to increase the storage limit.
+    *
+    * @param systemPath
+    *   [[java.lang.String systemPath]] of the JSON Lines file to be uploaded. <p> If the purpose is set to "fine-tune", each line is a JSON
+    *   record with "prompt" and "completion" fields representing your
+    *   [[https://platform.openai.com/docs/guides/fine-tuning/prepare-training-data training examples]].
+    * @param purpose
+    *   The intended purpose of the uploaded documents. <p> Use "fine-tune" for Fine-tuning. This allows OpenAI to validate the format of
+    *   the uploaded file.
+    * @return
+    *   Uploaded file's basic information.
+    */
+  def uploadFile(systemPath: String, purpose: String): Request[Either[ResponseException[String, Exception], FileData]] =
+    openApiAuthRequest
+      .post(OpenAIEndpoints.FilesEndpoint)
+      .multipartBody(
+        multipart("purpose", purpose),
+        multipartFile("file", Paths.get(systemPath).toFile)
+      )
+      .response(asJsonSnake[FileData])
+
+  /** Upload a file that contains document(s) to be used across various endpoints/features. Currently, the size of all the files uploaded by
+    * one organization can be up to 1 GB. Please contact OpenAI if you need to increase the storage limit.
+    *
+    * @param systemPath
+    *   [[java.lang.String systemPath]] of the JSON Lines file to be uploaded and the purpose is set to "fine-tune", each line is a JSON
+    *   record with "prompt" and "completion" fields representing your
+    *   [[https://platform.openai.com/docs/guides/fine-tuning/prepare-training-data training examples]].
+    * @return
+    *   Uploaded file's basic information.
+    */
+  def uploadFile(systemPath: String): Request[Either[ResponseException[String, Exception], FileData]] =
+    openApiAuthRequest
+      .post(OpenAIEndpoints.FilesEndpoint)
+      .multipartBody(
+        multipart("purpose", "fine-tune"),
+        multipartFile("file", Paths.get(systemPath).toFile)
       )
       .response(asJsonSnake[FileData])
 
