8e3278f0752dd27d5a66b7e9cdcaa5e5f1d88c46
Andrzej Bil <bil.andrzej0@gmail.com>
2023-04-24 14:08:13 +0200

    Implement create embeddings endpoint (#41)

--- core/src/main/scala/sttp/openai/OpenAi.scala
+++ core/src/main/scala/sttp/openai/OpenAi.scala
@@ -9,13 +9,15 @@ import sttp.openai.requests.completions.chat.ChatRequestBody.ChatBody
 import sttp.openai.requests.completions.chat.ChatRequestResponseData.ChatResponse
 import sttp.openai.requests.completions.edit.EditRequestBody.EditBody
 import sttp.openai.requests.completions.edit.EditRequestResponseData.EditResponse
+import sttp.openai.requests.embeddings.EmbeddingsRequestBody.EmbeddingsBody
+import sttp.openai.requests.embeddings.EmbeddingsResponseBody.EmbeddingResponse
 import sttp.openai.requests.files.FilesResponseData._
 import sttp.openai.requests.finetunes.FineTunesRequestBody
-import sttp.openai.requests.images.variations.ImageVariationsConfig
 import sttp.openai.requests.finetunes.FineTunesResponseData.{FineTuneResponse, GetFineTunesResponse}
 import sttp.openai.requests.images.ImageResponseData.ImageResponse
 import sttp.openai.requests.images.creation.ImageCreationRequestBody.ImageCreationBody
 import sttp.openai.requests.images.edit.ImageEditsConfig
+import sttp.openai.requests.images.variations.ImageVariationsConfig
 import sttp.openai.requests.models.ModelsResponseData.{ModelData, ModelsResponse}
 import sttp.openai.requests.audio.AudioResponseData.AudioResponse
 import sttp.openai.requests.audio.transcriptions.TranscriptionConfig
@@ -417,6 +419,17 @@ class OpenAi(authToken: String) {
       .get(OpenAIEndpoints.FineTunesEndpoint)
       .response(asJsonSnake[GetFineTunesResponse])
 
+  /** @param embeddingsBody
+    *   Embeddings request body.
+    * @return
+    *   An embedding vector representing the input text.
+    */
+  def createEmbeddings(embeddingsBody: EmbeddingsBody) =
+    openApiAuthRequest
+      .post(OpenAIEndpoints.EmbeddingsEndpoint)
+      .body(embeddingsBody)
+      .response(asJsonSnake[EmbeddingResponse])
+
   /** @param fineTuneId
     *   The ID of the fine-tune job.
     * @return
@@ -439,6 +452,7 @@ private object OpenAIEndpoints {
   val CompletionsEndpoint: Uri = uri"https://api.openai.com/v1/completions"
   val CreateImageEndpoint: Uri = ImageEndpointBase.addPath("generations")
   val EditEndpoint: Uri = uri"https://api.openai.com/v1/edits"
+  val EmbeddingsEndpoint: Uri = uri"https://api.openai.com/v1/embeddings"
   val EditImageEndpoint: Uri = ImageEndpointBase.addPath("edits")
   val FilesEndpoint: Uri = uri"https://api.openai.com/v1/files"
   val FineTunesEndpoint: Uri = uri"https://api.openai.com/v1/fine-tunes"
---
+++ core/src/test/scala/sttp/openai/requests/embeddings/EmbeddingsDataSpec.scala
@@ -0,0 +1,39 @@
+package sttp.openai.requests.embeddings
+
+import org.scalatest.EitherValues
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import sttp.openai.fixtures
+import sttp.openai.json.SttpUpickleApiExtension
+import EmbeddingsResponseBody._
+
+class EmbeddingsDataSpec extends AnyFlatSpec with Matchers with EitherValues {
+  "Given list files response as Json" should "be properly deserialized to case class" in {
+    // given
+    val listFilesResponse = fixtures.EmbeddingsFixture.jsonCreateEmbeddingsResponse
+    val expectedResponse = EmbeddingResponse(
+      `object` = "list",
+      data = Seq(
+        EmbeddingData(
+          `object` = "embedding",
+          index = 0,
+          embedding = Seq(
+            0.0023064255, -0.009327292, 0.015797347, -0.0077780345, -0.0046922187
+          )
+        )
+      ),
+      model = "text-embedding-ada-002-v2",
+      usage = Usage(
+        promptTokens = 8,
+        totalTokens = 8
+      )
+    )
+    // when
+    val givenResponse: Either[Exception, EmbeddingResponse] =
+      SttpUpickleApiExtension.deserializeJsonSnake[EmbeddingResponse].apply(listFilesResponse)
+
+    // then
+    givenResponse.value shouldBe expectedResponse
+  }
+
+}
