36e7405474412528fc43e47587073798f5643f2f
Adam Fish Rybicki <adamrybicki@adams-macbook-pro.home>
2023-04-27 13:40:36 +0200

    change roles to ADTs

--- core/src/main/scala/sttp/openai/requests/completions/chat/Message.scala
+++ core/src/main/scala/sttp/openai/requests/completions/chat/Message.scala
@@ -8,7 +8,7 @@ import ujson.Str
   * @param content
   *   The contents of the message.
   */
-case class Message(role: String, content: String, name: Option[String] = None)
+case class Message(role: Role, content: String, name: Option[String] = None)
 
 object Message {
   implicit val messageRW: SnakePickle.ReadWriter[Message] = SnakePickle.macroRW[Message]
@@ -26,18 +26,18 @@ object Role {
 
   val values: Set[Role] = Set(System, User, Assistant)
 
-  private val byName = values.map(role => (role.getClass.getSimpleName.stripSuffix("$") -> role)).toMap
+  private val byRoleValue = values.map(role => role.value -> role).toMap
 
-  implicit val roleRW: SnakePickle.ReadWriter[Role] = SnakePickle.readwriter[ujson.Value].bimap[Role](
-    {
-      case System => SnakePickle.writeJs(System.value)
-      case User => SnakePickle.writeJs(User.value)
-      case Assistant => SnakePickle.writeJs(Assistant.value)
-      case Custom(customRole) => SnakePickle.writeJs(customRole)
-    },
-    jsonValue => SnakePickle.read[ujson.Value](jsonValue) match {
-      case Str(value) => byName.getOrElse(value, throw new DeserializationException(new Exception(s"Could not serialize $value")))
-      case e => throw new DeserializationException(new Exception(s"Could not serialize: $e"))
-    }
-  )
+  implicit val roleRW: SnakePickle.ReadWriter[Role] = SnakePickle
+    .readwriter[ujson.Value]
+    .bimap[Role](
+      role => SnakePickle.writeJs(role.value),
+      jsonValue => {
+        println(jsonValue)
+        SnakePickle.read[ujson.Value](jsonValue) match {
+          case Str(value) => byRoleValue.getOrElse(value, throw new DeserializationException(new Exception(s"Could not serialize $value")))
+          case e          => throw new DeserializationException(new Exception(s"Could not serialize: $e"))
+        }
+      }
+    )
 }
