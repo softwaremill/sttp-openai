203d1d95fa311af820562819276cb5bf3ae66746
andrzej <bil.andrzej0@gmail.com>
2023-04-28 15:12:31 +0200

    wip

--- core/src/main/scala/sttp/openai/OpenAISyncClient.scala
+++ core/src/main/scala/sttp/openai/OpenAISyncClient.scala
@@ -1,3 +1,20 @@
-package sttp.openai class OpenAISyncClient {
+package sttp.openai
 
+import sttp.client4.{DefaultSyncBackend, SyncBackend}
+import sttp.openai.requests.models.ModelsResponseData.ModelsResponse
+
+class OpenAISyncClient private (authToken: String, backend: SyncBackend) {
+
+  private val openAI = new OpenAI(authToken)
+  def getModels: Either[String, ModelsResponse] =
+    openAI
+      .getModels
+      .send(backend)
+      .body
+  def close(): Unit = backend.close()
+}
+
+object OpenAISyncClient {
+  def apply(authToken: String) = new OpenAISyncClient(authToken, DefaultSyncBackend())
+  def apply(authToken: String, backend: SyncBackend) = new OpenAISyncClient(authToken, backend)
 }
