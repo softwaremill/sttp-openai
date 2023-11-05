af9139244766c25b97d551fed2facea9604f5a26
Mateusz Dybowski <matdybowski@gmail.com>
2023-11-05 20:13:37 +0100

    Move error fixtures to separate object

--- core/src/test/scala/sttp/openai/client/SyncClientSpec.scala
+++ core/src/test/scala/sttp/openai/client/SyncClientSpec.scala
@@ -4,47 +4,23 @@ import org.scalatest.EitherValues
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import sttp.client4._
-import sttp.model.StatusCode
 import sttp.model.StatusCode._
 import sttp.openai.OpenAIExceptions.OpenAIException
-import sttp.openai.OpenAIExceptions.OpenAIException._
 import sttp.openai.OpenAISyncClient
+import sttp.openai.fixtures.ErrorFixture
 import sttp.openai.requests.models.ModelsResponseData._
 
 class SyncClientSpec extends AnyFlatSpec with Matchers with EitherValues {
-  private val (message, typ, param, code) = ("Some error message.", "error_type", "null", "invalid_api_key")
-  private val errorResponse =
-    s"""
-      |{
-      |  "error": {
-      |    "message": "$message",
-      |    "type": "$typ",
-      |    "param": $param,
-      |    "code": "$code"
-      |  }
-      |}""".stripMargin
-  private val testData: Seq[(StatusCode, OpenAIException)] = List(
-    (TooManyRequests, new RateLimitException(Some(message), Some(typ), None, Some(code), HttpError(errorResponse, TooManyRequests))),
-    (BadRequest, new InvalidRequestException(Some(message), Some(typ), None, Some(code), HttpError(errorResponse, BadRequest))),
-    (NotFound, new InvalidRequestException(Some(message), Some(typ), None, Some(code), HttpError(errorResponse, NotFound))),
-    (
-      UnsupportedMediaType,
-      new InvalidRequestException(Some(message), Some(typ), None, Some(code), HttpError(errorResponse, UnsupportedMediaType))
-    ),
-    (Unauthorized, new AuthenticationException(Some(message), Some(typ), None, Some(code), HttpError(errorResponse, Unauthorized))),
-    (Forbidden, new PermissionException(Some(message), Some(typ), None, Some(code), HttpError(errorResponse, Forbidden))),
-    (Conflict, new TryAgain(Some(message), Some(typ), None, Some(code), HttpError(errorResponse, Conflict))),
-    (
-      ServiceUnavailable,
-      new ServiceUnavailableException(Some(message), Some(typ), None, Some(code), HttpError(errorResponse, ServiceUnavailable))
-    ),
-    (Gone, new APIException(Some(message), Some(typ), None, Some(code), HttpError(errorResponse, Gone)))
-  )
-  for ((statusCode, expectedError) <- testData)
+  for ((statusCode, expectedError) <- ErrorFixture.testData)
     s"Service response with status code: $statusCode" should s"return properly deserialized ${expectedError.getClass.getSimpleName}" in {
-      val syncBackendStub = DefaultSyncBackend.stub.whenAnyRequest.thenRespondWithCode(statusCode, errorResponse)
+      // given
+      val syncBackendStub = DefaultSyncBackend.stub.whenAnyRequest.thenRespondWithCode(statusCode, ErrorFixture.errorResponse)
       val syncClient = OpenAISyncClient(authToken = "test-token", backend = syncBackendStub)
+
+      // when
       val caught = intercept[OpenAIException](syncClient.getModels)
+
+      // then
       caught.getClass shouldBe expectedError.getClass
       caught.message shouldBe expectedError.message
       caught.cause shouldBe expectedError.cause
@@ -54,6 +30,7 @@ class SyncClientSpec extends AnyFlatSpec with Matchers with EitherValues {
     }
 
   "Fetching models with successful response" should "return properly deserialized list of available models" in {
+    // given
     val modelsResponse = sttp.openai.fixtures.ModelsGetResponse.singleModelResponse
     val syncBackendStub = DefaultSyncBackend.stub.whenAnyRequest.thenRespondWithCode(Ok, modelsResponse)
     val syncClient = OpenAISyncClient(authToken = "test-token", backend = syncBackendStub)
@@ -86,6 +63,8 @@ class SyncClientSpec extends AnyFlatSpec with Matchers with EitherValues {
         )
       )
     )
+
+    // when & then
     syncClient.getModels shouldBe deserializedModels
   }
 }
