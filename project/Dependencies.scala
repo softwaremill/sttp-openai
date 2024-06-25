f250354ee1fe078c543d58191c10b44c212c37be
scala-steward <scala-steward>
2024-06-25 00:05:38 +0000

    Update pekko-stream to 1.0.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val scalaTestCats = "1.5.0"
 
     val sttpClient = "4.0.0-M14"
-    val pekkoStreams = "1.0.2"
+    val pekkoStreams = "1.0.3"
     val akkaStreams = "2.6.20"
     val uPickle = "3.1.4"
   }
