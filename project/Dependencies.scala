ca3f8396baf88f140de67cd0e8dc62d0005316f3
scala-steward <scala-steward>
2024-09-11 00:06:32 +0000

    Update ox:core to 0.3.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -40,7 +40,7 @@ object Dependencies {
 
     val sttpClientOx = Seq(
       "com.softwaremill.sttp.client4" %% "ox" % V.sttpClient,
-      "com.softwaremill.ox" %% "core" % "0.3.7"
+      "com.softwaremill.ox" %% "core" % "0.3.8"
     )
 
     val uPickle = "com.lihaoyi" %% "upickle" % V.uPickle
