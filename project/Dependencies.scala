bab385159a666c965b7c3f466cb1575827826c6c
scala-steward <scala-steward>
2025-01-15 00:06:51 +0000

    Update client4:akka-http-backend, ... to 4.0.0-M24

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val scalaTestCats = "1.6.0"
 
     val sttpApispec = "0.11.5"
-    val sttpClient = "4.0.0-M19"
+    val sttpClient = "4.0.0-M24"
     val pekkoStreams = "1.1.3"
     val akkaStreams = "2.6.20"
     val tapir = "1.11.10"
