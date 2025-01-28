a930a56cf2cc319c666a4d7e5f7d6510a1f46e97
scala-steward <scala-steward>
2025-01-28 00:07:04 +0000

    Update apispec-model, jsonschema-circe to 0.11.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     val scalaTest = "3.2.19"
     val scalaTestCats = "1.6.0"
 
-    val sttpApispec = "0.11.5"
+    val sttpApispec = "0.11.6"
     val sttpClient = "4.0.0-M19"
     val pekkoStreams = "1.1.3"
     val akkaStreams = "2.6.20"
