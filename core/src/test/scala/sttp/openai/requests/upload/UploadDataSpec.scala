3092bf27c5064c7728ee80340eb4415faf3355f0
michal.zyga <michal.zyga@softwaremill.com>
2025-03-07 09:45:14 +0100

    add delete model method

--- core/src/test/scala/sttp/openai/requests/upload/UploadDataSpec.scala
+++ core/src/test/scala/sttp/openai/requests/upload/UploadDataSpec.scala
@@ -48,7 +48,7 @@ class UploadDataSpec extends AnyFlatSpec with Matchers with EitherValues {
       status = "completed",
       expiresAt = 1719127296,
       file = Some(
-        File(
+        FileMetadata(
           id = "file-xyz321",
           bytes = 1147483648,
           createdAt = 1719186911,
