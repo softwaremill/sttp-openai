616568933facfb6dfb8166fc70f1f8367835e21a
softwaremill-ci <ci@softwaremill.pl>
2025-06-05 13:42:36 +0200

    Update tapir-apispec-docs, ... to 1.11.33 (#348)
    Co-authored-by: scala-steward <scala-steward>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
     val sttpClient = "4.0.8"
     val pekkoStreams = "1.1.3"
     val akkaStreams = "2.6.20"
-    val tapir = "1.11.32"
+    val tapir = "1.11.33"
     val uPickle = "4.2.1"
   }
 
