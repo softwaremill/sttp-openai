e5cd6f2a764287cb09398746a76556417192e48a
softwaremill-ci <ci@softwaremill.pl>
2025-06-24 02:11:40 +0200

    Update client4:akka-http-backend, ... to 4.0.9 (#359)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val scalaTestCats = "1.6.0"
 
     val sttpApispec = "0.11.9"
-    val sttpClient = "4.0.8"
+    val sttpClient = "4.0.9"
     val pekkoStreams = "1.1.4"
     val akkaStreams = "2.6.20"
     val tapir = "1.11.34"
