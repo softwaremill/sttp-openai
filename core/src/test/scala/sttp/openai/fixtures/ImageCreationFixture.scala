b24f6049c1b7d760ef9c07081125650211521f79
Mateusz Piękos <mateuszpiekos@gmail.com>
2025-07-29 19:16:02 +0200

    Added missing fields to ImageCreationFixture

--- core/src/test/scala/sttp/openai/fixtures/ImageCreationFixture.scala
+++ core/src/test/scala/sttp/openai/fixtures/ImageCreationFixture.scala
@@ -3,19 +3,35 @@ package sttp.openai.fixtures
 object ImageCreationFixture {
   val jsonRequest: String = """{
       |"prompt": "cute fish",
+      |"background": "transparent",
       |"model": "dall-e-3",
+      |"moderation": "strict",
       |"n": 1,
+      |"output_compression": 80,
+      |"output_format": "png",
+      |"partial_images": 2,
+      |"quality": "high",
       |"size": "1024x1024",
       |"response_format": "url",
+      |"stream": false,
+      |"style": "vivid",
       |"user": "user1"
       |}""".stripMargin
 
   val jsonRequestDalle2: String = """{
       |"prompt": "cute fish",
+      |"background": "transparent",
       |"model": "dall-e-2",
+      |"moderation": "strict",
       |"n": 1,
+      |"output_compression": 80,
+      |"output_format": "png",
+      |"partial_images": 2,
+      |"quality": "high",
       |"size": "1024x1024",
       |"response_format": "url",
+      |"stream": false,
+      |"style": "vivid",
       |"user": "user1"
       |}""".stripMargin
 
