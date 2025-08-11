1728e807ebbcff6d76377adf74588a1ea31c2a25
Mateusz Piękos <mateuszpiekos@gmail.com>
2025-08-11 09:09:43 +0200

    Fixed get models response

--- core/src/test/scala/sttp/openai/fixtures/ModelsGetResponse.scala
+++ core/src/test/scala/sttp/openai/fixtures/ModelsGetResponse.scala
@@ -9,49 +9,13 @@ object ModelsGetResponse {
       |      "id":"babbage",
       |      "object":"model",
       |      "created":1649358449,
-      |      "owned_by":"openai",
-      |      "permission":[
-      |        {
-      |          "id":"modelperm-49FUp5v084tBB49tC4z8LPH5",
-      |          "object":"model_permission",
-      |          "created":1669085501,
-      |          "allow_create_engine":false,
-      |          "allow_sampling":true,
-      |          "allow_logprobs":true,
-      |          "allow_search_indices":false,
-      |          "allow_view":true,
-      |          "allow_fine_tuning":false,
-      |          "organization":"*",
-      |          "group":null,
-      |          "is_blocking":false
-      |        }
-      |      ],
-      |      "root":"babbage",
-      |      "parent":null
+      |      "owned_by":"openai"
       |    },
       |    {
       |      "id":"davinci",
       |      "object":"model",
       |      "created":1649359874,
-      |      "owned_by":"openai",
-      |      "permission":[
-      |        {
-      |          "id":"modelperm-U6ZwlyAd0LyMk4rcMdz33Yc3",
-      |          "object":"model_permission",
-      |          "created":1669066355,
-      |          "allow_create_engine":false,
-      |          "allow_sampling":true,
-      |          "allow_logprobs":true,
-      |          "allow_search_indices":false,
-      |          "allow_view":true,
-      |          "allow_fine_tuning":false,
-      |          "organization":"*",
-      |          "group":null,
-      |          "is_blocking":false
-      |        }
-      |      ],
-      |      "root":"davinci",
-      |      "parent":null
+      |      "owned_by":"openai"
       |    }
       |  ]
       |}""".stripMargin
@@ -63,25 +27,7 @@ object ModelsGetResponse {
                               |      "id":"babbage",
                               |      "object":"model",
                               |      "created":1649358449,
-                              |      "owned_by":"openai",
-                              |      "permission":[
-                              |        {
-                              |          "id":"modelperm-49FUp5v084tBB49tC4z8LPH5",
-                              |          "object":"model_permission",
-                              |          "created":1669085501,
-                              |          "allow_create_engine":false,
-                              |          "allow_sampling":true,
-                              |          "allow_logprobs":true,
-                              |          "allow_search_indices":false,
-                              |          "allow_view":true,
-                              |          "allow_fine_tuning":false,
-                              |          "organization":"*",
-                              |          "group":null,
-                              |          "is_blocking":false
-                              |        }
-                              |      ],
-                              |      "root":"babbage",
-                              |      "parent":null
+                              |      "owned_by":"openai"
                               |    }
                               |   ]
                               |}""".stripMargin
